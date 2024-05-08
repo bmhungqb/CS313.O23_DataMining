01fe873698eb11abfafb0e48a37252652e61cafd
bmhungqb <21522110@gm.uit.edu.vn>
2024-05-08 23:39:58 +0700

    init

--- secret_key.py
+++ secret_key.py
@@ -1,4 +1,4 @@
-openapi_key = "sk-proj-zvlXUU4DcoNzK2NTO69BT3BlbkFJseCsoPAh8R0H0WiLviCN"
+openapi_key = "YOUR SECRET KEY"
 serper_api_key = "YOUR SECRET KEY"
 google_api_key = "YOUR SECRET KEY"
 google_cse_id = "YOUR SECRET KEY"
